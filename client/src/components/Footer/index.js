b3d84ce4b21623f66db98e58d1736d25d666e15a
salwaMugh <Sam8O.o@hotmail.com>
2018-11-08 12:09:47 +0200

    fix eslint

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,34 @@
-import React, { Component } from "react";
-import "./index.css";
+import React, { Component } from 'react';
+import './index.css';
 
 const Footer = () => (
-    <div className="footer-container">
-
+  <div className="footer-container">
     <div className="logo">
-    <span className="name"><h1>chari<span className="colored-name">zone</span></h1></span>
-    <span className="description"><h2> all charities in one place</h2></span>
+      <span className="name">
+        <h1>
+          chari<span className="colored-name">zone</span>
+        </h1>
+      </span>
+      <span className="description">
+        <h2> all charities in one place</h2>
+      </span>
     </div>
 
-<div className="social-icons">
-<img src="https://files.gitter.im/ChariZone/Lobby/GCAl/facebook.png" alt=""/>
-<img src="https://files.gitter.im/ChariZone/Lobby/bqDl/twitter.png" alt=""/>
-<img src="https://files.gitter.im/ChariZone/Lobby/ByRj/linkedin.png" alt=""/>
-</div>
-
+    <div className="social-icons">
+      <img
+        src="https://files.gitter.im/ChariZone/Lobby/GCAl/facebook.png"
+        alt=""
+      />
+      <img
+        src="https://files.gitter.im/ChariZone/Lobby/bqDl/twitter.png"
+        alt=""
+      />
+      <img
+        src="https://files.gitter.im/ChariZone/Lobby/ByRj/linkedin.png"
+        alt=""
+      />
     </div>
-)
+  </div>
+);
 
 export default Footer;
