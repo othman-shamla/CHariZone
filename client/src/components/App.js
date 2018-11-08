81adc995298b6b004b97df44dc0e3259559c70bb
asmaa-ezz <you@example.com>
2018-11-08 13:32:59 +0200

    Import font-awesome@4.7.0

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import 'font-awesome/css/font-awesome.css';
 import './App.css';
 
 import HomePage from './HomePage';
