f953d63a2f7305396372bb6cbe88823253eccdca
asmaa-ezz <you@example.com>
2018-11-08 14:57:46 +0200

    Add the element card to countan info

--- client/src/components/SearchReaslt/CardResult/index.js
+++ client/src/components/SearchReaslt/CardResult/index.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './style.css';
 
-const CardResult = () => <h1>00</h1>;
+class CardResult extends Component {
+  render() {
+    const { key, name, text, isActive, logo, classification, website } = this.props;
+    return (
+      <div className="container" key={key}>
+        <div className="card-details">
+          <h1>{name}</h1>
+          <p className="information">" {text} "</p>
+          <div className="control">
+            <button
+              type="button"
+              className={isActive ? 'btn active' : 'btn'}
+              //   onClick={() => this.props.onClick()}
+            >
+              <span className="add">ADD</span>
+              <span className="add-cart">
+                <i className="fa fa-plus-square" aria-hidden="true" />
+              </span>
+              <span className="compare">Compare</span>
+            </button>
+          </div>
+        </div>
+        <div className="card-image">
+          <img src={logo} alt="Logo defult" />
+          <div className="info">
+            <h2>The Description</h2>
+            <ul>
+              <li>
+                <strong>Name: </strong>
+                {name}
+              </li>
+              <li>
+                <strong>Classification: </strong>
+                {classification}
+              </li>
+              <li>
+                <strong>Website: </strong>
+                {website}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default CardResult;
