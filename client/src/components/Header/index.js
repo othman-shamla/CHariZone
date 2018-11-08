3699d003375f7640c4eca77a63a3f602bac9c0d6
salwaMugh <Sam8O.o@hotmail.com>
2018-11-08 12:09:47 +0200

    fix eslint

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import "./index.css";
+import React, { Component } from 'react';
+import './index.css';
 
-
-class Header extends Component{
-
+class Header extends Component {
   render() {
     return (
-    <div className="header-container">
-
-    <div className="logo">
-    <span className="name"><h1>chari<span className="colored-name">zone</span></h1></span>
-    <span className="description"><h2> all charities in one place</h2></span>
-    </div>
-
-<div className="icon">
-</div>
-    </div>
+      <div className="header-container">
+        <div className="logo">
+          <span className="name">
+            <h1>
+              chari<span className="colored-name">zone</span>
+            </h1>
+          </span>
+          <span className="description">
+            <h2> all charities in one place</h2>
+          </span>
+        </div>
+        <div className="icon" />
+      </div>
     );
   }
 }
