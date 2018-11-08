98b31434a33328701086faf98cad1a9e56b6daad
asmaa-ezz <you@example.com>
2018-11-08 14:18:54 +0200

    Declrade Declare count = 0 in state

--- client/src/components/SearchReaslt/index.js
+++ client/src/components/SearchReaslt/index.js
@@ -15,7 +15,7 @@ class SearchReaslt extends Component {
         logo:
           'https://www.atlrewards.net/cwa-nearby-areas-portlet/images/nologo.png',
         text:
-          'TO HELP AND EDUCATE YOUNG PERSONS THROUGH THEIR LEISURE-TIME AND HOLIDAY ACTIVITIES'
+          'TO HELP AND EDUCATE YOUNG PERSONS THROUGH THEIR LEISURE-TIME AND HOLIDAY ACTIVITIES',
       },
       {
         id: 2,
@@ -26,7 +26,7 @@ class SearchReaslt extends Component {
         logo:
           'https://www.atlrewards.net/cwa-nearby-areas-portlet/images/nologo.png',
         text:
-          'THE CHARITY WORKS FOR THE PUBLIC BENEFIT HAVING AS ITS OBJECTS THE PROMOTION OF THE UPBRINGING AND CARE OF CHILDREN AND YOUNG PEOPLE IN NEED FROM ALL BACKGROUNDS, CULTURES, AND FAITHS OR NONE IN PARTICULAR BY:\r\n1. PROMOTING THEIR BETTER CARE AND SAFEGUARDING THEM;\r\n2. PROVIDING ASSISTANCE TO THEM AND THEIR FAMILIES AND CARERS;\r\n3. PROMOTING THEIR HEALTH;\r\n4. PROMOTING THEIR EDUCATION AND ESTABLISHMENT IN LIFE.'
+          'THE CHARITY WORKS FOR THE PUBLIC BENEFIT HAVING AS ITS OBJECTS THE PROMOTION OF THE UPBRINGING AND CARE OF CHILDREN AND YOUNG PEOPLE IN NEED FROM ALL BACKGROUNDS, CULTURES, AND FAITHS OR NONE IN PARTICULAR BY:\r\n1. PROMOTING THEIR BETTER CARE AND SAFEGUARDING THEM;\r\n2. PROVIDING ASSISTANCE TO THEM AND THEIR FAMILIES AND CARERS;\r\n3. PROMOTING THEIR HEALTH;\r\n4. PROMOTING THEIR EDUCATION AND ESTABLISHMENT IN LIFE.',
       },
       {
         id: 3,
@@ -37,7 +37,7 @@ class SearchReaslt extends Component {
         logo:
           'https://www.atlrewards.net/cwa-nearby-areas-portlet/images/nologo.png',
         text:
-          'TO PROMOTE THE CARE, UPBRINGING AND ESTABLISHMENT IN LIFE OF CHILDREN AND YOUNG PEOPLE WHO ARE OR HAVE BEEN LOOKED AFTER OR ASSISTED BY LOCAL AUTHORITIES OR OTHER PUBLIC OR CHARITABLE BODIES CHARGED WITH THE CARE & UPBRINGING OF CHILDREN OR WHO ARE OR HAVE BEEN IN THE CARE OF FOSTER OR ADOPTIVE PARENTS.'
+          'TO PROMOTE THE CARE, UPBRINGING AND ESTABLISHMENT IN LIFE OF CHILDREN AND YOUNG PEOPLE WHO ARE OR HAVE BEEN LOOKED AFTER OR ASSISTED BY LOCAL AUTHORITIES OR OTHER PUBLIC OR CHARITABLE BODIES CHARGED WITH THE CARE & UPBRINGING OF CHILDREN OR WHO ARE OR HAVE BEEN IN THE CARE OF FOSTER OR ADOPTIVE PARENTS.',
       },
       {
         id: 4,
@@ -48,7 +48,7 @@ class SearchReaslt extends Component {
         logo:
           'https://www.atlrewards.net/cwa-nearby-areas-portlet/images/nologo.png',
         text:
-          'To advance the christian faith in all or any part of the world.\rThe relief of poverty, suffering or distress amongst children.\rThe education and training of children.\rThe development of children to social and spiritual maturity.'
+          'To advance the christian faith in all or any part of the world.\rThe relief of poverty, suffering or distress amongst children.\rThe education and training of children.\rThe development of children to social and spiritual maturity.',
       },
       {
         id: 5,
@@ -62,6 +62,7 @@ class SearchReaslt extends Component {
           'We care for over 3,000 kids across the world but no two are the same, so we take care to understand and meet each of their individual needs, from food and healthcare, to loving parents and quality education. We help them reach their full potential, stand on their own two feet and change their futures. We?ve transformed thousands of kids? lives, and we are only just getting started.',
       },
     ],
+    count: 0,
   };
 
   render() {
