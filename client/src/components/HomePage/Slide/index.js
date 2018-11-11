9d063e5368e979e0b3f49dd31326447c746b845e
Ons Nader <onsmariposa-nana@hotmail.com>
2018-11-11 15:29:42 +0200

    prepared for the addition of slider

---
+++ client/src/components/HomePage/Slide/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Slide extends Component {
+  render() {
+    return <div className="slider" />;
+  }
+}
+export default Slide;
