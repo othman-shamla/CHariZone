a85d49abb12b8dcebbac619eb59e3dd8dfeb9ab0
Ons Nader <onsmariposa-nana@hotmail.com>
2018-11-11 11:38:44 +0200

    amended some eslint errors and style mistakes

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 
 import './index.css';
 import LandingPage from './LandingPage';
-class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        <LandingPage />
-      </div>
-    );
-  }
-}
 
+const HomePage = () => (
+  <div>
+    <LandingPage />
+  </div>
+);
 export default HomePage;
