09a5ac506c3410eb16796cbfcd13d9de78522d6f
Ons Nader <onsmariposa-nana@hotmail.com>
2018-11-11 16:02:59 +0200

    fixed class problem

--- client/src/components/HomePage/ContactUs/index.js
+++ client/src/components/HomePage/ContactUs/index.js
@@ -2,21 +2,18 @@ import React, { Component } from 'react';
 
 import './index.css';
 
-class ContactUS extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <section className="ending-section">
-          <div className="contact-us">
-            <h2>Contact US</h2>
-            <input type="text" placeholder="Title" className="input" />
-            <input type="text" placeholder="Email" className="input" />
-            <textarea placeholder="Message" className="input" />
-            <button type="button">Send</button>
-          </div>
-        </section>
-      </React.Fragment>
-    );
-  }
-}
+const ContactUS = () => (
+  <React.Fragment>
+    <section className="ending-section">
+      <div className="contact-us">
+        <h2>Contact US</h2>
+        <input type="text" placeholder="Title" className="input" />
+        <input type="text" placeholder="Email" className="input" />
+        <textarea placeholder="Message" className="input" />
+        <button type="button">Send</button>
+      </div>
+    </section>
+  </React.Fragment>
+);
+
 export default ContactUS;
