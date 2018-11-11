071de0eb4127a13185efc9a2c963e2db5251aedf
asmaa-ezz <you@example.com>
2018-11-11 14:43:17 +0200

    Conver the component class to function

--- client/src/components/SearchReaslt/CharityCount/index.js
+++ client/src/components/SearchReaslt/CharityCount/index.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 
 import './style.css';
 
-class CharityCount extends Component {
-  render() {
-    const { count } = this.props;
-    return (
-      <div className="count-charity">
-        <img
-          className="img-charity"
-          src="https://image.flaticon.com/icons/svg/1170/1170651.svg"
-          alt="img charity"
-        />
-        <div className="count">
-          <strong>{count}</strong>
-        </div>
+const CharityCount = props => {
+  const { count } = props;
+  return (
+    <div className="count-charity">
+      <img
+        className="img-charity"
+        src="https://image.flaticon.com/icons/svg/1170/1170651.svg"
+        alt="img charity"
+      />
+      <div className="count">
+        <strong>{count}</strong>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+// class CharityCount {
+//   render() {
+//     const { count } = this.props;
+//     return (
+//       <div className="count-charity">
+//         <img
+//           className="img-charity"
+//           src="https://image.flaticon.com/icons/svg/1170/1170651.svg"
+//           alt="img charity"
+//         />
+//         <div className="count">
+//           <strong>{count}</strong>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default CharityCount;
