229d48b450a694eba0d2b54ad16c8386a0b48c86
Ons Nader <onsmariposa-nana@hotmail.com>
2018-11-11 14:07:08 +0200

    added the second section in home page

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 import './index.css';
 import LandingPage from './LandingPage';
+import Works from './Works';
 
 const HomePage = () => (
   <div>
     <LandingPage />
+    <Works />
   </div>
 );
 export default HomePage;
