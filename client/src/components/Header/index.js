3771931d2507575591c8ac16f3522db73e1490ea
asmaa-ezz <you@example.com>
2018-11-12 09:41:36 +0200

    Add the nav and conver rhe a to link

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,21 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { Component } from 'react';
 import './index.css';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  state = {
+    toggle: false,
+  };
+
+  move() {
+    this.setState(prevState => ({
+      toggle: !prevState.toggle,
+    }));
+  }
+
   render() {
+    const { toggle } = this.state;
     return (
       <div className="header-container">
         <div className="div-logo">
@@ -15,7 +28,23 @@ class Header extends Component {
             <h2> all charities in one place</h2>
           </span>
         </div>
-        <div className="icon" />
+        <div>
+          <nav>
+            <div
+              className={toggle ? 'icon change' : 'icon'}
+              onClick={() => this.move()}
+              onKeyDown={this.handleKeyDown}
+            >
+              <div className="hamburger" />
+              <div className="items" id="items">
+                <Link to="/">HOW IT WORKS</Link>
+                <Link to="/">CATEGORIES</Link>
+                <Link to="/">ADVANCED SEARCH</Link>
+                <Link to="/">CONTACT US</Link>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     );
   }
