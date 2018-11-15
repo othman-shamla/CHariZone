6949060b3a15476a9e7d41fb84c54e9b1fcd5e48
asmaa-ezz <you@example.com>
2018-11-15 08:16:19 +0200

    Add the contener page category

---
+++ client/src/components/Category/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Category extends Component {
+  // state = {  }
+  render() {
+    return <h1>Category</h1>;
+  }
+}
+
+export default Category;
