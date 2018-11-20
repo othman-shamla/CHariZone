f6cbeb11d51753e54a1bbd63cf6d4477538d18a5
asmaa-ezz <you@example.com>
2018-11-20 14:48:34 +0200

    Add the airTable file to get all data in speshal data in search query Relates #13

---
+++ server/controllers/airTable.js
@@ -0,0 +1,63 @@
+const Airtable = require("airtable");
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: "keyXOKIUjFwpxFeeA"
+});
+const base = Airtable.base("appddZ82zrrbFkLQP");
+
+exports.get = (req, res) => {
+  const { incfrom, incto, exform, exto, category } = req.query;
+  let array = [];
+  let obj;
+
+  if (incfrom === "-1" && exform === "-1" && category === "-1") {
+    obj = {
+      view: "Grid view"
+    };
+  } else if (incfrom === "-1" && category === "-1") {
+    obj = {
+      filterByFormula: `AND(({expend} > ${exform}),({expend} < ${exto}))`
+    };
+  } else if (exform === "-1" && category === "-1") {
+    obj = {
+      filterByFormula: `AND(({income} > ${incfrom}),({income} < ${incto}))`
+    };
+  } else if (incfrom === "-1") {
+    obj = {
+      filterByFormula: `AND(({income} > ${incfrom}),({income} < ${incto}))`,
+      filterByFormula: `(FIND(${category},{What}))`
+    };
+  } else if (exform === "-1") {
+    obj = {
+      filterByFormula: `AND(({income} > ${incfrom}),({income} < ${incto}))`,
+      filterByFormula: `(FIND(${category},{What}))`
+    };
+  } else if (incfrom === "-1" && exform === "-1") {
+    obj = {
+      filterByFormula: `(FIND(${category},{What}))`
+    };
+  } else if (category === "-1") {
+    obj = {
+      filterByFormula: `AND(({expend} > ${exform}),({expend} < ${exto}))`,
+      filterByFormula: `AND(({income} > ${incfrom}),({income} < ${incto}))`
+    };
+  } else {
+    obj = {
+      filterByFormula: `AND(({expend} > ${exform}),({expend} < ${exto}))`,
+      filterByFormula: `AND(({income} > ${incfrom}),({income} < ${incto}))`,
+      filterByFormula: `(FIND(${category},{What}))`
+    };
+  }
+  base("Imported table")
+    .select(obj)
+    .firstPage(function(err, records) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      records.forEach(function(record) {
+        array.push(record.fields);
+      });
+      res.send({ data: array });
+    });
+};
