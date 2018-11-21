524ec6ad6e6744db9da09f6d0c90653dc72db2f2
asmaa-ezz <you@example.com>
2018-11-21 20:46:46 +0200

    Add the router page not found

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.css';
 import './App.css';
 
@@ -7,23 +7,22 @@ import HomePage from './HomePage';
 import SearchReaslt from './SearchReaslt';
 import CharityDetalis from './CharityDetalis';
 import Category from './Category';
-import Header from './Header';
-import Footer from './Footer';
 import Comparison from './Comparison';
 import Filter from './HomePage/LandingPage/Filter';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
-    <>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/search" component={SearchReaslt} />
-	      <Route exact path='/charity' component={CharityDetalis} />
-        <Route exact path="/category" component={Category} />
-        <Route exact path="/compare" component={Comparison} />
-        <Route exact path="/filter" component={Filter} />
-      </Switch>
-    </>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/search" component={SearchReaslt} />
+      <Route exact path="/charity" component={CharityDetalis} />
+      <Route exact path="/category" component={Category} />
+      <Route exact path="/compare" component={Comparison} />
+      <Route exact path="/filter" component={Filter} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
   </BrowserRouter>
 );
 export default App;
