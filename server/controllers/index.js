8b7ff920825d72bacac2ac2d5a837ee2ecd8d7e5
othman20 <othmansh2010@gmail.com>
2018-11-21 14:22:49 +0200

    solve conflict

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,14 +6,8 @@ const Search = require('./search');
 
 const router = express.Router();
 
-<<<<<<< Updated upstream
 router.get('/filter', AdvancedSearch.get);
 router.get('/charity/:id', Charity.get);
 router.get('/search', Search.get);
-=======
-router.get('/filter', airTable.filter);
-router.get('/charity/:id', airTable.charity);
-router.get('/search', airTable.search);
->>>>>>> Stashed changes
 
 module.exports = router;
