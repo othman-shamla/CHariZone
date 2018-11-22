65762fe320aaa5f9f7aabe9c45aaac391bb192e5
Ons Nader <onsmariposa-nana@hotmail.com>
2018-11-22 14:58:31 +0200

    missing 404 page found

--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -4,12 +4,12 @@ import './index.css';
 const NotFound = () => (
   <React.Fragment>
     <section className="notFoundSection">
-    <h1 className="notFoundTitle">404</h1>
-    <h2 className="notFoundTitleSub">Page not found</h2>
-    <div className="circle1" />
-    <div className="circle2" />
-    <div className="circle4" />
-    <div className="circle5" />
+      <h1 className="notFoundTitle">404</h1>
+      <h2 className="notFoundTitleSub">Page not found</h2>
+      <div className="circle1" />
+      <div className="circle2" />
+      <div className="circle4" />
+      <div className="circle5" />
     </section>
   </React.Fragment>
 );
