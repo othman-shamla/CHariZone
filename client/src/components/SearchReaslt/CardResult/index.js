bff82d13943e9cf9cf2724d5045dd639797e1de0
asmaa-ezz <you@example.com>
2018-11-26 11:39:27 +0200

    Return to the previous case Because your button has been disabled compared Relates #100

--- client/src/components/SearchReaslt/CardResult/index.js
+++ client/src/components/SearchReaslt/CardResult/index.js
@@ -19,47 +19,47 @@ class CardResult extends Component {
       onClick,
     } = this.props;
     return (
-      <Link className="link-card" to={`/charity/${idChirty}`}>
-        <div className="container" key={key0}>
-          <div className="card-details">
-            <h1>{name}</h1>
-            <p className="information">" {text} "</p>
-            <div className="control">
-              <button
-                type="button"
-                className={isActive ? 'btn active' : 'btn'}
-                onClick={() => onClick()}
-              >
-                <span className="add">ADD</span>
-                <span className="add-cart">
-                  <i className="fa fa-plus-square" aria-hidden="true" />
-                </span>
-                <span className="compare">Compare</span>
-              </button>
-            </div>
-          </div>
-          <div className="card-image">
-            <img src={logo} alt="Logo defult" />
-            <div className="info">
-              <h2>The Description</h2>
-              <ul>
-                <li>
-                  <strong>Name: </strong>
-                  {name}
-                </li>
-                <li>
-                  <strong>Classification: </strong>
-                  {classification}
-                </li>
-                <li>
-                  <strong>Website: </strong>
-                  {website}
-                </li>
-              </ul>
-            </div>
+      <div className="container" key={key0}>
+        <div className="card-details">
+          <Link className="link-card" to={`/charity/${idChirty}`}>
+            {name}
+          </Link>
+          <p className="information">" {text} "</p>
+          <div className="control">
+            <button
+              type="button"
+              className={isActive ? 'btn active' : 'btn'}
+              onClick={() => onClick()}
+            >
+              <span className="add">ADD</span>
+              <span className="add-cart">
+                <i className="fa fa-plus-square" aria-hidden="true" />
+              </span>
+              <span className="compare">Compare</span>
+            </button>
           </div>
         </div>
-      </Link>
+        <div className="card-image">
+          <img src={logo} alt="Logo defult" />
+          <div className="info">
+            <h2>The Description</h2>
+            <ul>
+              <li>
+                <strong>Name: </strong>
+                {name}
+              </li>
+              <li>
+                <strong>Classification: </strong>
+                {classification}
+              </li>
+              <li>
+                <strong>Website: </strong>
+                {website}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }
