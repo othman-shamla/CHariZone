163b4bec4a165e2b0e58343f93d130b4e0c9a002
Ons Nader <onsmariposa-nana@hotmail.com>
2018-11-28 10:17:30 +0200

    fixes to contactUs/home page and charity details profilr

--- client/src/components/CharityDetalis/BoxKpi/index.js
+++ client/src/components/CharityDetalis/BoxKpi/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './style.css';
 
 const BoxKpi = props => {
-  const { name, children } = props;
+  const { name, children, description } = props;
   return (
     <div className="kpi">
       <div className="kpi--box">
         <div className="kpi--titleBox">
           <span className="kpi--title">{name}</span>
+          <p className="kpi--description">{description}</p>
         </div>
         <div className="kpi--children">{children}</div>
       </div>
