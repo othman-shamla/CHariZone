51111314201232cd170c342fdec8449e24f76b59
othman20 <othmansh2010@gmail.com>
2018-12-05 13:06:18 +0200

    add AreaOfOperation component

--- client/src/components/CharityDetalis/Contact/index.js
+++ client/src/components/CharityDetalis/Contact/index.js
@@ -2,33 +2,7 @@
 import React from 'react';
 import './style.css';
 import 'react-circular-progressbar/dist/styles.css';
-
-const areaOfOperationRow = areaOfOperation => {
-  const halfLength = Math.floor(areaOfOperation.length / 2);
-  let areaOfOperationColumnOne = areaOfOperation;
-  let areaOfOperationColumnTwo = [];
-  if (areaOfOperation.length > 6) {
-    areaOfOperationColumnOne = areaOfOperation.slice(0, halfLength);
-    areaOfOperationColumnTwo = areaOfOperation.slice(
-      halfLength,
-      areaOfOperation.length
-    );
-  }
-  return (
-    <>
-      <div className="header--items">
-        {areaOfOperationColumnOne.map((x, i) => (
-          <span key={x + i}>{x}</span>
-        ))}
-      </div>
-      <div className="header--items">
-        {areaOfOperationColumnTwo.map((x, i) => (
-          <span key={x + i}>{x}</span>
-        ))}
-      </div>
-    </>
-  );
-};
+import areaOfOperationRow from './AreaOfOperation';
 
 const Contact = props => {
   const {
